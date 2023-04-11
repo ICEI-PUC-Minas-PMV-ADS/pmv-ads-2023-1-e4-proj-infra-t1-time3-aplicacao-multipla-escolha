4c58c89e902e607df72bce25850b5c001aa4f301
Sergio Menezes <sergio.filho.444859@sga.pucminas.br>
2023-04-11 17:49:37 -0300

    Add Controller para Turmas

---
+++ backend/apiSql/multipla-escolha-api-sql/Controllers/TurmasController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using multipla_escolha_api_sql.Models;
+
+namespace multipla_escolha_api_sql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurmasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TurmasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var model = await _context.Turmas.ToListAsync();
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Turma model)
+        {
+            _context.Turmas.Add(model);
+            await _context.SaveChangesAsync();
+            
+            return Ok(model);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var model = await _context.Turmas.FirstOrDefaultAsync(t => t.Id == id);
+
+            if (model == null) NotFound();
+
+            return Ok(model);
+        }
+    }
+}
