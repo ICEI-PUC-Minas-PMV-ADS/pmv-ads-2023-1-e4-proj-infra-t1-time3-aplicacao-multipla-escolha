9260a46a56347ff8935f3169fa08f0549c9659a5
Álvaro <91163177+alvaroalfaya@users.noreply.github.com>
2023-05-21 19:58:28 -0300

    Atualizando editarturma page

--- frontend/multipla-escolha/src/pages/editarTurma.js
+++ frontend/multipla-escolha/src/pages/editarTurma.js
@@ -1,28 +1,19 @@
-import React, {useState, useEffect, useContext} from 'react';
-import Navbar from '../components/navbar';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from "react";
+import Navbar from "../components/navbar";
+import { Link } from "react-router-dom";
 
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-import EditarTurmaFormComponent from '../components/editarTurmaFormComponent';
+import EditarTurmaFormComponent from "../components/editarTurmaFormComponent";
 
+function EditarTurma() {
+  const params = useParams();
 
-function EditarTurma() {  
-
-    const params = useParams();
-
-    return (
-        <div>
-            <Navbar />
-            <div className='d-flex flex-column container'>
-                <div className="m-auto mt-4">
-                    <h1>Editar turma</h1>
-                </div>
-                <div>
-                    <EditarTurmaFormComponent idTurma={params.id}/>
-                </div> 
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <EditarTurmaFormComponent idTurma={params.id} />
+    </div>
+  );
 }
 export default EditarTurma;
