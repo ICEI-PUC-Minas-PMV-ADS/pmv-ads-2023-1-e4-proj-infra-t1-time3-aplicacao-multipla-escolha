c4f8368cd28827f6e2b82f1064016a5ce273c1d2
Sergio Menezes <sergio.filho.444859@sga.pucminas.br>
2023-06-09 18:00:25 -0300

    Add tela de minhas turmas professor

---
+++ backend/Api/multipla-escolha-api/Migrations/20230608202711_M15.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace multipla_escolha_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class M15 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resultados_Usuarios_AlunoId",
+                table: "Resultados");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resultados_Usuarios_AlunoId",
+                table: "Resultados",
+                column: "AlunoId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resultados_Usuarios_AlunoId",
+                table: "Resultados");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resultados_Usuarios_AlunoId",
+                table: "Resultados",
+                column: "AlunoId",
+                principalTable: "Usuarios",
+                principalColumn: "Id");
+        }
+    }
+}
